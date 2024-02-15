4977ba06b84ddb56f0f059c3a14e92307d6ee9a7
Alex Su <alsu@itu.dk>
2024-02-15 16:01:08 +0100

    Exercise 02.1

--- libraries/itugl/src/ituGL/shader/Shader.cpp
+++ libraries/itugl/src/ituGL/shader/Shader.cpp
@@ -44,7 +44,7 @@ void Shader::SetSource(std::span<const char*> source)
     const char** sources = source.data();
 
     // (todo) 02.1: Set the shader source code
-
+    glShaderSource(GetHandle(), static_cast<int>(source.size()), source.data(), nullptr);
 }
 
 // Compile the shader source code
@@ -55,7 +55,7 @@ bool Shader::Compile()
     Handle handle = GetHandle();
 
     // (todo) 02.1: Compile the shader
-
+    glCompileShader(GetHandle());
 
     return IsCompiled();
 }
