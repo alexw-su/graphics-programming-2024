16257e22fedff34c7100132cd796a5f6a46d4077
Alex Su <alsu@itu.dk>
2024-02-21 21:06:30 +0100

    Exercise 02.5

--- exercises/exercise02/ParticlesApplication.cpp
+++ exercises/exercise02/ParticlesApplication.cpp
@@ -16,19 +16,19 @@ struct Particle
     float size;
     float birth;
     float duration;
- 
+    Color color;
 };
 
 // List of attributes of the particle. Must match the structure above
-const std::array<VertexAttribute, 4> s_vertexAttributes =
+const std::array<VertexAttribute, 5> s_vertexAttributes =
 {
     VertexAttribute(Data::Type::Float, 2), // position
+    
     // (todo) 02.X: Add more vertex attributes
-
     VertexAttribute(Data::Type::Float, 1), // size
     VertexAttribute(Data::Type::Float, 1), // birth
     VertexAttribute(Data::Type::Float, 1), // duration
-
+    VertexAttribute(Data::Type::Float, 4), // color
 };
 
 
@@ -78,8 +78,9 @@ void ParticlesApplication::Update()
         // (todo) 02.X: Compute new particle attributes here
         float size = RandomRange(10.0f, 30.0f);
         float duration = RandomRange(1.0f, 2.0f);
+        Color color = RandomColor();
 
-        EmitParticle(mousePosition, size, duration);
+        EmitParticle(mousePosition, size, duration, color);
     }
 
     // save the mouse position (to compare next frame and obtain velocity)
@@ -155,7 +156,7 @@ void ParticlesApplication::InitializeShaders()
     }
 }
 
-void ParticlesApplication::EmitParticle(const glm::vec2& position, float size, float duration)
+void ParticlesApplication::EmitParticle(const glm::vec2& position, float size, float duration, Color color)
 {
     // Initialize the particle
     Particle particle;
@@ -165,6 +166,7 @@ void ParticlesApplication::EmitParticle(const glm::vec2& position, float size, f
     particle.size = size;
     particle.birth = GetCurrentTime();
     particle.duration = duration;
+    particle.color = color;
 
     // Get the index in the circular buffer
     unsigned int particleIndex = m_particleCount % m_particleCapacity;
--- exercises/exercise02/shaders/particles.vert
+++ exercises/exercise02/shaders/particles.vert
@@ -4,15 +4,18 @@ layout (location = 0) in vec2 ParticlePosition;
 layout (location = 1) in float ParticleSize;
 layout (location = 2) in float ParticleBirth;
 layout (location = 3) in float ParticleDuration;
+layout (location = 4) in vec4 ParticleColor;
 
 // (todo) 02.5: Add Color output variable here
-
+out vec4 Color;
 
 // (todo) 02.X: Add uniforms
 uniform float CurrentTime;
 
 void main()
 {
+	Color = ParticleColor;
+
 	float age = CurrentTime - ParticleBirth;
 
 	gl_Position = vec4(ParticlePosition, 0.0, 1.0);
